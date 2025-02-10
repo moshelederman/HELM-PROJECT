58287a9600906aa616d15ef219f1c10f97501edf
moshelederman <mos9972@gmail.com>
2025-02-10 12:42:50 +0200

    a

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 2
   node_config {
     machine_type = "n1-standard-1"
+    disk_size_gb = 50
+    disk_type    = "pd-standard"
   }
 }
 
